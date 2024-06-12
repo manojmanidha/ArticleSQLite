2a70490db3907416f6f1581cccd61274d94a8dc9
Manoj KS <manojks.062001@gmail.com>
2024-06-12 09:27:12 +0530

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 const articleRoutes = require('./routes/articleRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
